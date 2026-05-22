8b8cbc234e3344c3f2294bb58c79946727d7ad99
BigDave07 <daveed3k3k@gmail.com>
2026-05-21 23:44:28 -0600

    Adding Maximum Array Solution

---
+++ arrays/maximum_subarray.py
@@ -0,0 +1,15 @@
+
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        current_sum = nums[0]
+
+        for n in nums[1:]:
+
+            current_sum = max(n, current_sum + n) #
+            max_sum = max(max_sum, current_sum)
+        return max_sum
+    
+# TimeComplexity = O(n)
+# SpaceComplexity = O(1)
